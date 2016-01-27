dfaf756f5cec694937f813316775f23533454b5e
Aaron Lun <alun@wehi.edu.au>
2016-01-27 17:17:16 +0000

    Added code to generate human marker set.

--- inst/exdata/generate_markers.R
+++ inst/exdata/generate_markers.R
@@ -7,3 +7,24 @@ load("pairs_functions.RData")
 
 all.pairs <- sandbag(is.G1=id.G1, is.S=id.S, is.G2M=id.G2M, training.data[genes.training,], fraction=0.5)
 saveRDS(file="mouse_cycle_markers.rds", all.pairs)
+
+# HUMAN:
+
+count.file <- "GSE64016.csv.gz"
+download.file("http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE64016&format=file&file=GSE64016%5FH1andFUCCI%5Fnormalized%5FEC%2Ecsv%2Egz", count.file)
+hs.counts <- read.csv(count.file, header=TRUE, row.names=1)
+hs.G1 <- grepl("G1", colnames(hs.counts))
+hs.S <- grepl("S", colnames(hs.counts))
+hs.G2 <- grepl("G2", colnames(hs.counts))
+
+library(org.Hs.eg.db)
+anno <- select(org.Hs.eg.db, keytype="SYMBOL", key=rownames(hs.counts), column="ENSEMBL")
+m <- match(anno$SYMBOL, rownames(hs.counts))
+hs.counts2 <- as.matrix(hs.counts[m,])
+rownames(hs.counts2) <- anno$ENSEMBL
+hs.counts2 <- hs.counts2[!is.na(anno$ENSEMBL),]
+hs.cycle <- select(org.Hs.eg.db, keytype="GOALL", key="GO:0007049", column="ENSEMBL")
+hs.training <- rownames(hs.counts2) %in% hs.cycle$ENSEMBL
+
+all.pairs <- sandbag(is.G1=hs.G1, is.S=hs.S, is.G2M=hs.G2, hs.counts2[hs.training,], fraction=0.5)
+saveRDS(file="human_cycle_markers.rds", all.pairs)
