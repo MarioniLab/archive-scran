84f338f4e08eb9873fbbeecd92fed9f225b12fbd
Aaron Lun <alun@wehi.edu.au>
2016-02-10 09:48:59 +0000

    Streamlined shuffling code.

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,5 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
-#include <cmath>
 
 struct Rx_random_seed {
     Rx_random_seed() {
@@ -15,7 +14,7 @@ template <typename Pt>
 void Rx_shuffle (Pt start, Pt end) {
     size_t n=(end-start), i;
     for (i=n-1; i>0; --i) {
-        std::swap(start[i], start[int(std::floor(unif_rand()*(i+1)))]); // Equal chance of getting anything from [0, i].
+        std::swap(start[i], start[int(unif_rand()*(i+1))]); // Equal chance of getting anything from [0, i], as per random_shuffle in C++.
     }
     return;
 }
