4e104ed8d402f9eef5fa5bdf8f5dd080db8b6a01
Aaron Lun <alun@wehi.edu.au>
2016-03-02 13:14:05 +0000

    Added checks for silly inputs.

--- tests/testthat/test-sandbag.R
+++ tests/testthat/test-sandbag.R
@@ -1,6 +1,6 @@
 # This tests the sandbag function, by running it and checking that the selecter markers make sense.
 
-require(scran); require(testthat);
+# require(scran); require(testthat); source("test-sandbag.R")
 
 happycheck <- function(X1, X2, X3, pairings, frac) {
     is.okay <- logical(nrow(pairings))
@@ -8,9 +8,9 @@ happycheck <- function(X1, X2, X3, pairings, frac) {
     thresh2 <- ceiling(ncol(X2) * frac)
     thresh3 <- ceiling(ncol(X3) * frac)
     for (p in seq_along(is.okay)) {
-         diff1 <- X1[pairings[p,1],] - X1[pairings[p,2],]
-         diff2 <- X2[pairings[p,1],] - X2[pairings[p,2],]
-         diff3 <- X3[pairings[p,1],] - X3[pairings[p,2],]
+         diff1 <- X1[pairings$first[p],] - X1[pairings$second[p],]
+         diff2 <- X2[pairings$first[p],] - X2[pairings$second[p],]
+         diff3 <- X3[pairings$first[p],] - X3[pairings$second[p],]
          
          u1 <- sum(diff1 > 0)
          u2 <- sum(diff2 > 0)
@@ -44,6 +44,25 @@ XG1 <- X[,is.G1,drop=FALSE]
 XS <- X[,is.S,drop=FALSE]
 XG2M <- X[,is.G2M,drop=FALSE]
 
+expect_true(all(happycheck(XG1, XS, XG2M, out$G1, frac)))
+expect_true(all(happycheck(XG2M, XS, XG1, out$G2M, frac)))
+expect_true(all(happycheck(XS, XG1, XG2M, out$S, frac)))
+
+# Checking silly inputs.
+
+out <- sandbag(X[0,], is.G1, is.S, is.G2M, fraction=frac)
+expect_identical(out$G1, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
+expect_identical(out$G2M, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
+expect_identical(out$S, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
+expect_error(sandbag(X, integer(0), is.S, is.G2M, fraction=frac), "each phase must have at least one cell")
+
+is.G1 <- 1
+is.G2M <- 2
+is.S <- 3
+out <- sandbag(X, is.G1, is.S, is.G2M, fraction=frac)
+XG1 <- X[,is.G1,drop=FALSE]
+XS <- X[,is.S,drop=FALSE]
+XG2M <- X[,is.G2M,drop=FALSE]
 expect_true(all(happycheck(XG1, XS, XG2M, out$G1, frac)))
 expect_true(all(happycheck(XG2M, XS, XG1, out$G2M, frac)))
 expect_true(all(happycheck(XS, XG1, XG2M, out$S, frac)))
@@ -100,4 +119,12 @@ XG2M <- counts(X[,is.G2M,drop=FALSE])
 expect_true(all(happycheck(XG1, XS, XG2M, out$G1, frac)))
 expect_true(all(happycheck(XG2M, XS, XG1, out$G2M, frac)))
 expect_true(all(happycheck(XS, XG1, XG2M, out$S, frac)))
+
+# Testing for a SCESet, with all spike-ins.
+
+isSpike(X) <- TRUE
+out <- sandbag(X, is.G1, is.S, is.G2M, fraction=frac)
+expect_identical(out$G1, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
+expect_identical(out$G2M, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
+expect_identical(out$S, data.frame(first=character(0), second=character(0), stringsAsFactors=FALSE))
 
