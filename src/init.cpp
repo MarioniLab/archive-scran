7eb9ac9f86aae478efa16d0609aa6859065dea9d
Aaron Lun <alun@wehi.edu.au>
2016-06-07 11:53:57 +0100

    Updated C++ registration for modified functions.

--- src/init.cpp
+++ src/init.cpp
@@ -4,15 +4,16 @@
 extern "C" {
 
 static const R_CallMethodDef all_call_entries[] = {
-    REGISTER(forge_system, 6),
+    REGISTER(forge_system, 4),
     REGISTER(shuffle_scores, 8),
     REGISTER(get_null_rho, 2),
     REGISTER(get_null_rho_design, 3),
-    REGISTER(compute_rho, 4),
+    REGISTER(compute_rho, 3),
     REGISTER(auto_shuffle, 2),
     REGISTER(compute_cordist, 2),
     REGISTER(subset_and_divide, 3),
     REGISTER(estimate_variance, 4),
+    REGISTER(rank_subset, 4),
     {NULL, NULL, 0}
 };
 
