556d9f6bce14bdcafad9b7c63dd28efa0dad3076
Aaron Lun <alun@wehi.edu.au>
2016-06-30 15:06:58 +0100

    Simplified cyclone C++ code.

--- src/init.cpp
+++ src/init.cpp
@@ -5,7 +5,7 @@ extern "C" {
 
 static const R_CallMethodDef all_call_entries[] = {
     REGISTER(forge_system, 4),
-    REGISTER(shuffle_scores, 7),
+    REGISTER(shuffle_scores, 8),
     REGISTER(get_null_rho, 2),
     REGISTER(get_null_rho_design, 3),
     REGISTER(compute_rho, 3),
--- src/scran.h
+++ src/scran.h
@@ -18,7 +18,7 @@ extern "C" {
 
 SEXP forge_system (SEXP, SEXP, SEXP, SEXP);
 
-SEXP shuffle_scores (SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP shuffle_scores (SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 SEXP get_null_rho (SEXP, SEXP);
 
