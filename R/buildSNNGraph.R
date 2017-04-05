1e338574ce751b2e0e4209a7a847f13b89840c0a
Aaron Lun <alun@wehi.edu.au>
2017-04-05 18:53:32 +0100

    Switched to C++ for efficient SNN graph building. Added tests, updated docs for buildSNNGraph.

--- R/buildSNNGraph.R
+++ R/buildSNNGraph.R
@@ -4,26 +4,18 @@
 # the shared nearest neighbours of the two cells, as described in the SNN-Cliq paper.
 #
 # written by Aaron Lun
-# created 3 April 2017    
+# created 3 April 2017
+# last modified 5 April 2017    
 { 
     if (!is.null(subset.row)) {
         x <- x[.subset_to_index(subset.row, x, byrow=TRUE),]
     }
     nn.out <- get.knn(t(x), k=k, algorithm="cover_tree") 
-    nn.index <- nn.out$nn.index
-    all.nodes <- row(nn.out$nn.index)
-    edges <- as.vector(rbind(as.integer(all.nodes), as.integer(nn.index)))
 
-    weights <- matrix(0, nrow=nrow(all.nodes), ncol=ncol(all.nodes))
-    for (i in seq_len(nrow(all.nodes))) {
-        i.nn <- c(i, nn.index[i,])
-        for (x in seq_len(k)) {
-            j <- i.nn[x+1]
-            j.nn <- c(j, nn.index[j,])
-            shared <- intersect(i.nn, j.nn) 
-            weights[i, x] <- k - 0.5 * min(match(shared, i.nn) + match(shared, j.nn))
-        }
-    }
+    g.out <- .Call(cxx_build_snn, nn.out$nn.index)
+    if (is.character(g.out)) { stop(g.out) }
+    edges <- g.out[[1]] 
+    weights <- g.out[[2]]
 
     g <- make_graph(edges, directed=FALSE)
     E(g)$weight <- weights
