66692d1d65538fced63230e0cf3fd9425cae5f8d
Aaron Lun <alun@wehi.edu.au>
2017-07-11 15:19:32 +0100

    Minor refactoring to trendVar.

--- R/trendVar.R
+++ R/trendVar.R
@@ -1,12 +1,12 @@
-.trend_var <- function(x, trend=c("loess", "semiloess"), 
-                       span=0.3, family="symmetric", degree=1,
-                       start=NULL, design=NULL, subset.row=NULL)
+.trend_var <- function(x, parametric=TRUE, method=c("loess", "spline", "semiloess"), 
+                       span=0.3, family="symmetric", degree=1, df=4,
+                       old=FALSE, start=NULL, design=NULL, subset.row=NULL)
 # Fits a polynomial trend to the technical variability of the log-CPMs,
 # against their abundance (i.e., average log-CPM).
 # 
 # written by Aaron Lun
 # created 21 January 2016
-# last modified 6 June 2017
+# last modified 11 July 2017
 {
     subset.row <- .subset_to_index(subset.row, x, byrow=TRUE)
     checked <- .make_var_defaults(x, fit=NULL, design=design)
@@ -21,35 +21,43 @@
     is.okay <- vars > 1e-8
     kept.vars <- vars[is.okay]
     kept.means <- means[is.okay]
+
+    method <- match.arg(method) 
+    if (method=="semiloess") {
+        warning("'semiloess' is deprecated, use parametric=TRUE and method='loess' instead")
+        method <- "loess"
+        parametric <- TRUE
+    }
    
-    # Fitting a trend (parametric+loess or full loess).
-    trend <- match.arg(trend) 
-    if (trend=="semiloess") {
+    # Fitting a parametric curve to try to flatten the shape.
+    if (parametric) { 
         if (length(kept.vars) <= 3L) {
             stop("need at least 4 values for non-linear curve fitting")
         } 
 
-        # Fitting a parametric curve to try to flatten the shape.
         if (is.null(start)) start <- .get_nls_starts(kept.vars, kept.means)
         init.fit <- nls(kept.vars ~ (a*kept.means)/(kept.means^n + b), start=start,
                         control=nls.control(warnOnly=TRUE, maxiter=500), algorithm="port",
                         lower=list(a=0, b=0, n=1))
 
-        # Refitting a loess to the residuals of the logs.
-        kept.resids <- log2(kept.vars/fitted(init.fit))
-        loess.fit <- loess(kept.resids ~ kept.means, degree=degree, family=family, span=span)
-        SUBFUN <- function(x) { 
-            predict(init.fit, data.frame(kept.means=x)) * 2^predict(loess.fit, data.frame(kept.means=x)) 
-        }
+        to.fit <- log(kept.vars/fitted(init.fit))
+        SUBSUBFUN <- function(x) { predict(init.fit, data.frame(kept.means=x)) }
     } else {
-        kept.lvars <- log2(kept.vars)
-        loess.fit <- loess(kept.lvars ~ kept.means, degree=degree, family=family, span=span)
-        SUBFUN <- function(x) { 2^predict(loess.fit, data.frame(kept.means=x)) }
+        to.fit <- log(kept.vars)
+        SUBSUBFUN <- function(x) { 1 }
+    } 
+    
+    # Fitting loess or splines to the remainder.
+    if (method=="loess") { 
+        after.fit <- loess(to.fit ~ kept.means, degree=degree, family=family, span=span)
+    } else {
+        after.fit <- MASS::rlm(to.fit ~ ns(kept.means, df=df))
     }
+    SUBFUN <- function(x) { exp(predict(after.fit, data.frame(kept.means=x))) * SUBSUBFUN(x) }
 
     # Estimating the df2, as well as scale shift from estimating mean of logs (assuming shape of trend is correct).
     leftovers <- kept.vars/SUBFUN(kept.means)
-    f.fit <- fitFDistRobustly(leftovers, df1=nrow(design) - QR$rank)
+    f.fit <- fitFDistRobustly(leftovers, df1=nrow(design) - ncol(design))
     f.df2 <- f.fit$df2
     
     # We don't just want the scaling factor, we want the scaled mean of the F-distribution (see explanation below).
@@ -58,10 +66,10 @@
     } else if (f.df2 > 2) {
         f.scale <- f.fit$scale * f.df2/(f.df2 - 2) 
     } else {
-        warning("undefined expectation for small df2, setting scale to unity")
-        f.scale <- 1
+        warning("undefined expectation for small df2")
+        f.scale <- f.fit$scale
     }
-
+    
     # Creating a predictive function, with special behaviour at the ends.
     left.edge <- min(kept.means)
     left.val <- SUBFUN(left.edge) * f.scale
@@ -152,3 +160,4 @@ setMethod("trendVar", "SCESet", function(x, subset.row=NULL, ..., assay="exprs",
 #    E(var) = s0 * df2/(df2 - 2)
 #           = exp[E(log(var))] * 1/exp[E(log(F(df1, df2)))] * df2/(df2 - 2)
 # ... the first term of which is 2^predict, and the product of the latter two terms is 'f.scale'.
+
